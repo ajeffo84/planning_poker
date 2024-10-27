74952fd7626dea8da48c30c11601cbabd5ea7770
Andrew Jefferson <Andrew_Jefferson@me.com>
2024-10-27 09:59:14 +0000

    Connection to Backend

--- src/socket.ts
+++ src/socket.ts
@@ -1,7 +1,7 @@
 import { io } from 'socket.io-client';
 
-const SOCKET_URL = import.meta.env.PROD 
-  ? 'https://your-production-server.com'  // Replace with your production server URL
-  : 'http://localhost:3001';
+const SOCKET_URL = import.meta.env.PROD
+  ? import.meta.env.VITE_SERVER_URL
+  : import.meta.env.VITE_SERVER_URL //'http://localhost:3001';
 
 export const socket = io(SOCKET_URL);
